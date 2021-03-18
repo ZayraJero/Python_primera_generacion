53e4c7d730de0ec9a0aca582990a14ddd757e6bd
Zayra Jeronimo <zayrajero@gmail.com>
2021-03-17 19:15:24 -0600

    ADDED templates

--- dogtor/dogtor/urls.py
+++ dogtor/dogtor/urls.py
@@ -1,5 +1,5 @@
 """dogtor URL Configuration
-
+​
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -14,8 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("vet/", include("vet.urls"))]
